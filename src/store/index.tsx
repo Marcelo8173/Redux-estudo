1d041c4b638008ca56d049dc20f846d032bb4a96
Marcelo8173 <marceloandrebio@gmail.com>
2020-09-02 23:53:41 -0400

    criando um reducer

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,7 +1,8 @@
 //aqui vão estar todas as informaçoes globais
 
 import {createStore} from 'redux';
+import rootReducer from './modules/rootReducer';
 
-const store = createStore(() => []); // eu consigo escolher qual dado eu quero monitorar
+const store = createStore(rootReducer); // eu consigo escolher qual dado eu quero monitorar
 
 export default store;

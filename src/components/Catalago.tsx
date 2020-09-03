bd587d08a8ea9e9cb9bdca37c4cfc059023c760e
Marcelo8173 <marceloandrebio@gmail.com>
2020-09-03 00:10:46 -0400

    server-json

--- src/components/Catalago.tsx
+++ src/components/Catalago.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { IProduct } from '../store/modules/cart/types';
+import api from '../services/api';
+import { response } from 'express';
 
-const Catalago = () => {
+const Catalago = () => {    
+    const [catalog, setCatalog] = useState<IProduct[]>([]);
 
-    const catalogo = useSelector(state => state);
+    useEffect(() => {
+        api.get('products').then(response => {
+            setCatalog(response.data);
+        })
+    },[])
+
+    // const catalogo = useSelector(state => state);
     // const store = useStore();
 
     // console.log(store.getState()); //pegando os estados
 
     return(
-        <h1>Catalago</h1>
+        <>
+            <h1>Catalago</h1>
+            <div>
+                {catalog.map(item => (
+                    <article key={item.id}>
+                        <strong>{item.title}: </strong>
+                        <span>{item.price} </span>
+                        <button>Comprar</button>
+                    </article>
+                ))}
+            </div>
+        </>
     )
 }
 
